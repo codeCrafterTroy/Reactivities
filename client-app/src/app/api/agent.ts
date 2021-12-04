68139f109c52738f97d4cc74830764c5f788d7e1
Troy West <troy.west@codecrafters.co.nz>
2021-12-04 14:58:06 +1300

    End of section 6

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -9,13 +9,14 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
-axios.interceptors.response.use(response => {
-    sleep(1000).then(() => {
+axios.interceptors.response.use(async response => {
+    try { 
+        await sleep(1000);
         return response;
-    }).catch((error)=>{
+    }catch(error){
         console.log(error);
         return Promise.reject(error);
-    });
+    };
 })
 const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
@@ -30,7 +31,11 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities')
+    list: () => requests.get<Activity[]>('/activities'),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => axios.post<void>('/activities', activity),
+    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
 const agent ={
