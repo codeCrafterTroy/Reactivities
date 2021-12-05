f15bf77ef297311f4007608d4d74e45405345317
Troy West <troy.west@codecrafters.co.nz>
2021-12-05 13:16:16 +1300

    End of section 8

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Button, Card, Image } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { useStore } from '../../../app/stores/store';
 
 
-export default function ActivityDetails() {
-    const {activityStore} = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectActivity} = activityStore;
+export default observer(function ActivityDetails() {
+    const { activityStore } = useStore();
+    const { selectedActivity: activity, loadActivity, loadingInitial } = activityStore;
+    const {id} = useParams<{id: string}>();
 
-    if(!activity) return <LoadingComponent />;
+    useEffect(() => {
+        if(id) loadActivity(id);
+    }, [id, loadActivity]);
+
+    if (loadingInitial || !activity) return <LoadingComponent />;
 
     return (
         <Card fluid>
@@ -23,13 +31,11 @@ export default function ActivityDetails() {
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <a>
-                    <Button.Group widths='2'>
-                        <Button onClick={()=>openForm(activity.id)} basic color='blue' content='Edit' />
-                        <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
-                    </Button.Group>
-                </a>
+                <Button.Group widths='2'>
+                    <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit' />
+                    <Button as={Link} to='/activities' basic color='grey' content='Cancel' />
+                </Button.Group>
             </Card.Content>
         </Card>
     );
-}
+})
